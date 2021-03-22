a79eb169ff40e8398fcfa58a14875e5025b43251
Mijail Hernandez <mijailhernandez@gmail.com>
2021-03-22 11:44:27 -0500

    added images like base64.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 import "../scss/main.scss";
+import logo from "../images/logo.svg";
+import bgPatternCircles from "../images/bg-pattern-circles.svg";
+import bgPatternIntro from "../images/bg-pattern-intro.svg";
+import favicon from "../images/favicon-32x32.png";
+import iconArrowDark from "../images/icon-arrow-dark.svg";
+import iconArrowLight from "../images/icon-arrow-light.svg";
+import iconClose from "../images/icon-close.svg";
+import iconHamburger from "../images/icon-hamburger.svg";
+import illustrationEditorDesktop from "../images/illustration-editor-desktop.svg";
+import illustrationEditorMobile from "../images/illustration-editor-mobile.svg";
+import illustrationLaptopDesktop from "../images/illustration-laptop-desktop.svg";
+import illustrationLaptopMobile from "../images/illustration-laptop-mobile.svg";
+import illustrationPhones from "../images/illustration-phones.svg";
 
-console.log("hola mundo");
+// console.log(logo);
+
+document.querySelector(".navbar").firstElementChild.setAttribute("src", logo);
--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,20 @@ module.exports = {
     module: {
         rules: [
             {
-                use: ["style-loader", "css-loader", "sass-loader"],
                 test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
             },
             {
-                type: "asset",
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            }
+                type: 'asset',
+                // generator: {
+                //     filename: 'images/[hash][ext][query]'
+                // }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset',
+            },
         ]
     },
     plugins: [new HtmlWebpackPlugin({
