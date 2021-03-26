37c7bcdf3a715e861a8aace86424ff26799b5c53
Mijail Hernandez <mijailhernandez@gmail.com>
2021-03-26 13:14:09 -0500

    added navbar styles for desktop.

--- src/js/menu.js
+++ src/js/menu.js
@@ -10,7 +10,7 @@ menuIcon.addEventListener('click', () => {
 
 menu.firstElementChild.addEventListener('click', (e)=> {
     if (e.target.className === 'navbar__menu-item') {
-        e.target.firstElementChild.classList.toggle('js-rotate-arrow');
+        e.target.firstElementChild.firstElementChild.nextElementSibling.classList.toggle('js-rotate-arrow');
         e.target.nextElementSibling.classList.toggle('js-show-submenu');
     }
 })
