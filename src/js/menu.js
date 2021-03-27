54b726ae8c260f85784799e9be44c5559c6ebea4
Mijail Hernandez <mijailhernandez@gmail.com>
2021-03-26 22:13:12 -0500

    added header styles for desktop.

--- src/js/menu.js
+++ src/js/menu.js
@@ -9,7 +9,7 @@ menuIcon.addEventListener('click', () => {
 })
 
 menu.firstElementChild.addEventListener('click', (e)=> {
-    if (e.target.className === 'navbar__menu-item') {
+    if (e.target.className === 'navbar__menu-title') {
         e.target.firstElementChild.firstElementChild.nextElementSibling.classList.toggle('js-rotate-arrow');
         e.target.nextElementSibling.classList.toggle('js-show-submenu');
     }
